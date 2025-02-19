4b482824057a3d938b007adfcc1ae4182043a5e1
KimJin <xnxl159@naver.com>
2025-02-19 17:08:34 +0900

    save

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -31,53 +31,114 @@ public:
     {
         Node* newNode = new Node;
 
-        if (head == nullptr || tail == nullptr)
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (head == nullptr)
         {
             head = newNode;
             tail = newNode;
-
-            newNode->data = data;
-            newNode->next = nullptr;
-            newNode->previous = nullptr;
-
         }
         else
         {
-            while (head->previous != nullptr && tail->next != nullptr)
-            {
-                newNode->next = newNode->previous;
-                newNode->previous = nullptr;
+            head->previous = newNode;
+            newNode->next = head;
 
-                newNode->previous = newNode->next;
-                newNode->next = nullptr;
-            }
-
-                newNode->data = data;
+            head = newNode;
         }
 
         size++;
     }
 
-    void push_back(T data)
+    void pop_front()
     {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
 
+            if (head == tail)
+            {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else
+            {
+                deleteNode->next->previous = nullptr;
+
+                head = head->next;
+            }
+            delete deleteNode;
+
+            size--;
+        }
     }
 
+    void Show()
+    {
+        Node* currentNode = head;
 
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
 
+            currentNode = currentNode->next;
+        }
+    }
 
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (tail == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->previous = tail;
+
+            tail = newNode;
+        }
+
+        size++;
+    }
 
     ~DoubleLinkedList()
     {
-
+        while (head != nullptr)
+        {
+            pop_front();
+        }
     }
 
 };
 
 int main()
 {
-    
+    DoubleLinkedList<int> doubleLinkedList;
 
+    doubleLinkedList.push_front(30);
+    doubleLinkedList.push_front(20);
+    doubleLinkedList.push_front(10);
+
+    doubleLinkedList.pop_front();
+
+    doubleLinkedList.push_back(50);
+    doubleLinkedList.push_back(60);
+
+    doubleLinkedList.Show();
+    
 
     return 0;
 }
