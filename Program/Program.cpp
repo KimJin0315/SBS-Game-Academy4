fad7efe3693d423ee7adb164f1373e36c817fa1e
KimJin0315 <xnxl159@naver.com>
2025-03-06 19:46:02 +0900

    (2025.03.06) 레드 블럭 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -23,7 +23,84 @@ public:
 
     void push(T data)
     {
+        if (index + 1 >= SIZE)
+        {
+            cout << "Priority Queue Overflow" << endl;
+        }
+        else
+        {
+            container[++index] = data;
 
+            int child = index;
+            int parent = child / 2;
+
+            while (child > 1)
+            {
+                if (container[parent] < container[child])
+                {
+                    std::swap(container[parent], container[child]);
+                }
+
+                child = parent;
+                parent = child / 2;
+            }
+
+        }
+    }
+
+    const T & top ()
+    {
+        return container[1];
+    }
+
+    const bool & empty()
+    {
+        if (index <= 0 )
+        {
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void pop()
+    {
+        if (index <= 0)
+        {
+            cout << "Priority QUeue is Empty" << endl;
+        }
+        else
+        {
+            container[1] = container[index];
+            
+            container[index--] = NULL;
+
+            int parent = 1;
+
+            while (parent * 2 <= index)
+            {
+                int child = parent * 2;
+
+                if (container[child] < container[child + 1])
+                {
+                    child++;
+                }
+
+                if (container[child] < container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(container[parent], container[child]);
+
+                    parent = child;
+                }
+            }
+        }
     }
 
 };
@@ -31,6 +108,21 @@ public:
 int main()
 {
     PriorityQueue <int> priorityQueue;
+
+    priorityQueue.push(10);
+    priorityQueue.push(5);
+    priorityQueue.push(19);
+    priorityQueue.push(15);
+    priorityQueue.push(7);
+    priorityQueue.push(8);
+
+    while (priorityQueue.empty() == false)
+    {
+        cout << priorityQueue.top() << endl;
+
+        priorityQueue.pop();
+    }
+
 
     return 0;
 }
